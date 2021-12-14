59c4be45b08c896dfea16f50015474685d317eb2
nativ kolisher <nativk9@gmail.com>
2021-12-14 22:51:14 +0200

    adds rook class and functions

--- ChessProject/ChessProject/Rook.h
+++ ChessProject/ChessProject/Rook.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Tool.h"
+//#include <iostream>
+#define POSSIBLE_MOVES 14
 
 class Rook : public Tool
 {
 public:
     Rook(std::string pos, char type);
     int move(std::string _dst, Tool t);
+    void set_valid_moves(std::string pos);
+private:
+    std::string valid_moves[POSSIBLE_MOVES];
 };
