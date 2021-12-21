0dbc3f83bf460a3e72bb8b583e1a126c5fd213ff
nativ kolisher <nativk9@gmail.com>
2021-12-21 12:33:10 +0200

    add documantation & adding to class Game

--- ChessProject/ChessProject/Game.h
+++ ChessProject/ChessProject/Game.h
@@ -3,7 +3,13 @@
 #include <iostream>
 #include <string>
 #include "Board.h"
+#include <algorithm>
 //#include <tool.h>
+
+
+using std::cout;
+using std::endl;
+using std::string;
 
 class Game
 {
@@ -22,6 +28,17 @@ public:
 	void set_black_check(const bool b);
 	void set_white_check(const bool w);
 	void add_turn(const int t);
+
+	/*
+	Functions
+	*/
+	int check_tool(Tool t, Board& b, Game& g, string adress_dst, string adress_src, bool turn, char king_check);
+	static std::vector<string> change_pawn_vector(Pawn p, Board b);
+	static std::vector<string> change_vector(std::vector<string> rook_valid_moves, Board b, Tool t);
+	static std::vector<string> get_enemy_valid_moves(bool turn, Board b);
+	static int check_check(std::vector<string> valid_moves, char king_check, Board b, bool turn, Game& g);
+	
+
 
 private:
 	//fields
